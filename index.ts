963b47e4a4fa18067f7b3d8964750590e647f680
reysua <reymundojr.sua@avensia.com>
2018-09-10 17:04:15 +0800

    this is the commit

--- index.ts
+++ index.ts
@@ -3,11 +3,18 @@ import command from './command';
 async function  init(){
     try {
         await command('git fetch --all');
-        const {stdout} = await command('git rev-parse --abbrev-ref HEAD');
-        console.log('branch name:', stdout);
+        const {stdout: branchName} = await command('git rev-parse --abbrev-ref HEAD');
+        await command('git add .');
+        await command('git commit -m "this is the commit"');
+
+        await command('git checkout develop');
+        await command('git pull --rebase');
+        await command(`git checkout ${branchName}`);
+        await command('git rebase develop');
     } catch (e) {
         console.error('an error baby', e);
     }
 }
 
+// check
 init();
